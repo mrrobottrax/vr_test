26dcb811b6df10d2414df535d8e463f9d64997f6
mrrobottrax <59744880+mrrobottrax@users.noreply.github.com>
2024-07-27 23:01:26 -0400

    Clenup

--- src/gl/GlDebug.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifdef GL_ASSERTS
-#define glAssertSkip(expr, expected) if (expr != expected) throw std::runtime_error(std::format("GL expected {} got {:x} \nGL: {}", #expected, expr, (const char*)gluErrorString(glGetError())));
-#define glCheckErrors() if (glGetError()) { gluErrorString(glGetError()); }
-#else
-#define glAssertSkip(expr, expected)
-#define glCheckErrors()
-#endif // GL_ASSERTS
---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+#include "SDL3/SDL.h"
+#include "SDL3/SDL_main.h"
+
+int main(int argc, char* argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_Log("SDL_Init failed (%s)", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
+
+    if (SDL_CreateWindowAndRenderer("SDL issue", 640, 480, 0, &window, &renderer) < 0) {
+        SDL_Log("SDL_CreateWindowAndRenderer failed (%s)", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    while (1) {
+        int finished = 0;
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                finished = 1;
+                break;
+            }
+        }
+        if (finished) {
+            break;
+        }
+
+        SDL_SetRenderDrawColor(renderer, 80, 80, 80, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+    return 0;
+}
