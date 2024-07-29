3649a868e0556975a4df082204b5f17d22f30ae9
mrrobottrax <59744880+mrrobottrax@users.noreply.github.com>
2024-07-28 20:50:41 -0400

    sdl stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
-#include "SDL3/SDL.h"
-#include "SDL3/SDL_main.h"
+#include "SDL2/SDL.h"
 
 int main(int argc, char* argv[])
 {
@@ -10,36 +9,6 @@ int main(int argc, char* argv[])
         SDL_Log("SDL_Init failed (%s)", SDL_GetError());
         return 1;
     }
-
-    SDL_Window* window = NULL;
-    SDL_Renderer* renderer = NULL;
-
-    if (SDL_CreateWindowAndRenderer("SDL issue", 640, 480, 0, &window, &renderer) < 0) {
-        SDL_Log("SDL_CreateWindowAndRenderer failed (%s)", SDL_GetError());
-        SDL_Quit();
-        return 1;
-    }
-
-    while (1) {
-        int finished = 0;
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_EVENT_QUIT) {
-                finished = 1;
-                break;
-            }
-        }
-        if (finished) {
-            break;
-        }
-
-        SDL_SetRenderDrawColor(renderer, 80, 80, 80, SDL_ALPHA_OPAQUE);
-        SDL_RenderClear(renderer);
-        SDL_RenderPresent(renderer);
-    }
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
 
     SDL_Quit();
     return 0;
